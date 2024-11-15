2a28c18688e6fb9ce73f9f8f2533bc9bbeef2d4f
kolya <ngrimalyak@mail.ru>
2024-11-15 14:44:30 +0300

    Tnitial commit: create simple calculator

--- Program.cs
+++ Program.cs
@@ -1,15 +1,40 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ConsoleApp1
+class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введите первое число:");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите операцию (+, -, *, /):");
+        string operation = Console.ReadLine();
+
+        Console.WriteLine("Введите второе число:");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double result = 0;
+
+        switch (operation)
         {
+            case "+":
+                result = num1 + num2;
+                break;
+            case "-":
+                result = num1 - num2;
+                break;
+            case "*":
+                result = num1 * num2;
+                break;
+            case "/":
+                result = num1 / num2;
+                break;
+            default:
+                Console.WriteLine("Неизвестная операция");
+                break;
         }
+
+        Console.WriteLine($"Результат: {result}");
+        Console.ReadLine();
     }
 }
